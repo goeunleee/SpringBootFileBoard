7b0719f3b62e0471566b2961a8be20238e82824e
goeunleee <goeun3501@gamil.com>
2021-06-04 17:58:36 +0900

    comment

--- src/main/java/com/board/mapper/CommentMapper.java
+++ src/main/java/com/board/mapper/CommentMapper.java
@@ -6,15 +6,19 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.board.domain.CommentDTO;
 
-
+@Mapper
 public interface CommentMapper {
-	
-	
-	 public int insertComment(CommentDTO params);
-     public CommentDTO selectCommentDetail(Long idx);
-     public int updateComment(CommentDTO params);
-     public int deleteComment(Long idx);
-     public List<CommentDTO> selectCommentList (CommentDTO params);
-     public int selectCommentTotalCount(CommentDTO params);
+
+	public int insertComment(CommentDTO params);
+
+	public CommentDTO selectCommentDetail(Long idx);
+
+	public int updateComment(CommentDTO params);
+
+	public int deleteComment(Long idx);
+
+	public List<CommentDTO> selectCommentList(CommentDTO params);
+
+	public int selectCommentTotalCount(CommentDTO params);
 
 }
